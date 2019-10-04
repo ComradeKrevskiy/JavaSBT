779012845e2d63b9d65ffe299bbcc79d66192c8e
ComradeKrevskiy <47554662+ComradeKrevskiy@users.noreply.github.com>
2019-10-04 15:52:49 +0300

    Update MySuperPlugin.java

--- Task1_ClassLoader_2.0/src/MySuperPlugin.java
+++ Task1_ClassLoader_2.0/src/MySuperPlugin.java
@@ -3,6 +3,6 @@ import Plugin.Plugin;
 public class MySuperPlugin implements Plugin {
     @Override
     public void doUseful(){
-        System.out.println("MySuperPlugin.MySuperPlugin in BrowserPlugins is doing something useful");
+        System.out.println("MySuperPlugin in BrowserPlugins is doing something useful");
     };
 }
